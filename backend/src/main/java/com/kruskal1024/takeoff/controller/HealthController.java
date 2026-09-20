c3ff159a4f0d30b98c9fe2c9242ec22e7bbb294d
Primenaty Chigorimbo <primenatychigorimbo1@gmail.com>
2026-09-21 00:41:42 +0200

    fix: simplify health endpoint

--- backend/src/main/java/com/kruskal1024/takeoff/controller/HealthController.java
+++ backend/src/main/java/com/kruskal1024/takeoff/controller/HealthController.java
@@ -1,13 +1,15 @@
 package com.kruskal1024.takeoff.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HealthController {
 
-    @GetMapping("/api/health")
+    @GetMapping("/health")
     public String health() {
-        return "{\"status\":\"UP\"}";
+        return "OK";
     }
 }
